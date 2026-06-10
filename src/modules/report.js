30888191335dd0e5585a27b4c643f911dca09b60
Nishad Thalhath <nishad@users.noreply.github.com>
2026-06-11 03:06:02 +0900

    chore: import shared prefix table in report and add deno lint task

--- src/modules/report.js
+++ src/modules/report.js
@@ -18,23 +18,7 @@
  */
 
 import { datatypes, descRefs } from "./io.js";
-
-// ---------------------------------------------------------------------------
-// Standard prefix table (mirrors dsp.js STANDARD_PREFIXES)
-// ---------------------------------------------------------------------------
-
-const STANDARD_PREFIXES = {
-  dc: "http://purl.org/dc/elements/1.1/",
-  dcterms: "http://purl.org/dc/terms/",
-  foaf: "http://xmlns.com/foaf/0.1/",
-  skos: "http://www.w3.org/2004/02/skos/core#",
-  xl: "http://www.w3.org/2008/05/skos-xl#",
-  rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-  rdfs: "http://www.w3.org/2000/01/rdf-schema#",
-  owl: "http://www.w3.org/2002/07/owl#",
-  xsd: "http://www.w3.org/2001/XMLSchema#",
-  schema: "https://schema.org/",
-};
+import { STANDARD_PREFIXES } from "./prefixes.js";
 
 // ---------------------------------------------------------------------------
 // Flavor label definitions
